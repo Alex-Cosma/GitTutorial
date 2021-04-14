e7d051f6b83d03980391f8cb018daab8a5b76ad6
Alex <alexx.cosma@gmail.com>
2021-04-14 14:55:00 +0300

    - commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("new feature");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
+        // cod
     }
 }
