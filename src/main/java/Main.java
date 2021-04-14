ff7484476bb08138889ff534f1af2d8be11f4213
Alex <alexx.cosma@gmail.com>
2021-04-14 14:47:42 +0300

    - new code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("cod de pe branch");
+        System.out.println("new feature");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
