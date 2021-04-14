75ba7133ece34177c3fad66edce52be83bca859c
Alex <alexx.cosma@gmail.com>
2021-04-14 16:31:14 +0300

    removed

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("scriu ceva");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
-        // bla bla
-    }
-}
