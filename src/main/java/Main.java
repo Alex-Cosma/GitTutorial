5015290452df37b0fbc0e6105454e210534b4f37
Cosma Alex <Alex-Cosma@users.noreply.github.com>
2021-04-14 17:25:45 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        // commented out useless call
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
+        /// whatever
     }
 }
