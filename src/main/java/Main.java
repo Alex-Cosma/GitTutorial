6cad044e4d3367af353cd9e6cfc8879f23011e55
Alex <alexx.cosma@gmail.com>
2021-04-14 17:25:31 +0300

    - changed main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // commented out useless call
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
-        }
+        System.out.println("whatever");
     }
 }
